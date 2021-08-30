325bdaf038bfd955cbda9ab29bb270d40f80d6bd
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2021-08-30 09:30:11 -0300

    MIgracao do curriculo

---
+++ Migrations/20210830122401-create-curriculo.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Curriculos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      habilidades: {
+        type: Sequelize.STRING
+      },
+      nota: {
+        type: Sequelize.FLOAT
+      },
+      experiencia: {
+        type: Sequelize.INTEGER
+      },
+      formacao: {
+        type: Sequelize.STRING
+      },
+      nivel: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Curriculos');
+  }
+};
